1b7c03e53169a12e5f04f4dba94874bbaf80fb19
KYKong97 <124419374+KYKong97@users.noreply.github.com>
2023-03-07 14:20:05 +0800

    Update advertising.py

--- advertising.py
+++ advertising.py
@@ -24,7 +24,7 @@ def user_input_features():
 
 df = user_input_features()
 
-raw_df = pd.read_csv('/content/Advertising.csv')
+raw_df = pd.read_csv('Advertising.csv')
 
 st.subheader('User Input parameters')
 st.write(df)
